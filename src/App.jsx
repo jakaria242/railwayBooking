a85f6a3d97c539c4805559488dae82ded402e7f1
jakaria242 <jakaria.dev242@gmail.com>
2024-05-11 09:55:22 +0600

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/login/Login";
+import Registration from "./pages/registration/Registration";
+import Home from "./pages/home/Home";
+import ShowTicket from "./pages/showTicket/ShowTicket";
+import ErrorPage from "./pages/errorPage/ErrorPage";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+     <Route>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/registration" element={<Registration/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/showticket" element={<ShowTicket/>}/>
+      <Route path="/*" element={<ErrorPage/>}/>
+     </Route>
+    )
+  );
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
---
+++ src/pages/showTicket/ShowTicket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowTicket = () => {
+  return (
+    <div>ShowTicket</div>
+  )
+}
+
+export default ShowTicket
